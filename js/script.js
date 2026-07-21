b7ac333410768095ba5e416eb5fa3ca023ca811a
Tagoresatya <tagoresatya2022@gmail.com>
2026-07-22 00:15:16 +0530

    Fixed navbar toggle and updated portfolio styling

--- js/script.js
+++ js/script.js
@@ -2,22 +2,28 @@ console.log("Portfolio Loaded Successfully!");
 
 const topBtn = document.getElementById("topBtn");
 
-window.onscroll = function () {
+if (topBtn) {
 
-    if (document.documentElement.scrollTop > 300) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
-    }
+    window.onscroll = function () {
 
-};
+        if (document.documentElement.scrollTop > 300) {
+            topBtn.style.display = "block";
+        } else {
+            topBtn.style.display = "none";
+        }
 
-topBtn.onclick = function () {
-    window.scrollTo({
-        top:0,
-        behavior:"smooth"
-    });
-};
+    };
+
+    topBtn.onclick = function () {
+
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        });
+
+    };
+
+}
 
 
 // -----Scroll------
